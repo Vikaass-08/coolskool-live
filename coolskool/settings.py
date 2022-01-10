1e865337a07ef0cad6c3fa96f5f9d5e8aeb0273f
Vikaass-08 <pathakvikas190@gmail.com>
2022-01-10 16:03:28 +0530

    msg

--- coolskool/settings.py
+++ coolskool/settings.py
@@ -129,13 +129,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_URL = '/static/images/'
+MEDIA_URL = '/images/'
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STATIC_ROOT = BASE_DIR / 'static'
 MEDIA_ROOT = BASE_DIR / 'static/images'
 
 # STATIC_ROOT =
