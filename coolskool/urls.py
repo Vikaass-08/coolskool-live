3095ea611fea0219d8f036470d55e333047ec97b
Vikaass-08 <pathakvikas190@gmail.com>
2022-01-10 11:30:37 +0530

    api change

--- coolskool/urls.py
+++ coolskool/urls.py
@@ -8,8 +8,6 @@ from django.views.static import serve
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('base.urls')),
-    path('api/', include('base.api.urls')),
-
     url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
