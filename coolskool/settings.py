fa9f9919f5480fe3e5f102173b833ac9e7944253
Vikaass-08 <pathakvikas190@gmail.com>
2022-01-11 18:45:35 +0530

    mchanges

--- coolskool/settings.py
+++ coolskool/settings.py
@@ -131,11 +131,13 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'static'
-]
-
-STATIC_ROOT = BASE_DIR / 'static'
+if DEBUG:
+    STATICFILES_DIRS = [
+        BASE_DIR / 'static'
+    ]
+else:
+    STATIC_ROOT = BASE_DIR / 'static'
+    
 MEDIA_ROOT = BASE_DIR / 'static/images/'
 
 # STATIC_ROOT =
